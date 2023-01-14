6f5fdd9d408328f5a1072a5f7cde4e217d55d1be
Nasghul01 <nasrul01arsyad@gmail.com>
2023-01-14 15:06:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ict602_project/ui/about/AboutFragment.java
@@ -0,0 +1,37 @@
+package com.example.ict602_project.ui.about;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.ict602_project.databinding.FragmentNotificationsBinding;
+
+public class AboutFragment extends Fragment {
+
+    private FragmentNotificationsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        AboutViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(AboutViewModel.class);
+
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textNotifications;
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
